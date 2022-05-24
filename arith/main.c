4e3f008b48bdffdb1f1cf14b0cdffaccd401d177
MADHUSUDHAN AMUDALA <amadhusudhan2016@gmail.com>
2022-05-24 22:34:15 +0530

    Update main.c

--- arith/main.c
+++ arith/main.c
@@ -1,7 +1,11 @@
 #include<stdio.h>
+
 int main(int argv, char const *argc[])
 {
-int a,b,sum;
-printf("arith demo\n")
+int a,b,result;
+printf("arith demo\n");
+result=add(a,b);
+printf("addition Result :%d\n",result);
 
+return 0;
 }
