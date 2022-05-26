ab285f7013f8f6479db1b427a5eca8fb4c949d56
MADHUSUDHAN AMUDALA <amadhusudhan2016@gmail.com>
2022-05-26 18:47:33 +0530

    created makefile

---
+++ arith/arm/main.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int add(int a,int b);
+int main(int argv, char const *argc[])
+{
+int a,b,result;
+printf("arith demo\n");
+result=add(a,b);
+printf("addition Result :%d\n",result);
+
+return 0;
+}
