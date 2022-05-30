d3cb4fb149bc9ef12fa878f3051cf9b17f8e2b6d
amudalamadhusudhan <amadhusudhan2016@gmail.com>
2022-05-30 17:53:26 +0530

    sessions

---
+++ sessions/sessionsdemo.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int a,b=10;
+int main(int argc,char const *argv[])
+{
+int result =0;
+int *p=NULL;
+p= (int *)malloc(sizeof(int));
+
+    return 0;
+}
+int add (int a ,int b)
+{
+return a+b;
+
+}
