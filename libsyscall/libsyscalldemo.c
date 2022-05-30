3f1baf16fcc80166f9345e0d77310fd1f0bf5a92
amudalamadhusudhan <amadhusudhan2016@gmail.com>
2022-05-30 20:55:13 +0530

    systemcall demo

---
+++ libsyscall/libsyscalldemo.c
@@ -0,0 +1,17 @@
+
+
+
+#include<stdio.h>
+#include<string.h>
+char str[]="madhusudhan";
+int main(int argc, char const *argv[])
+{
+int b,a;
+scanf("%d",&a);
+b=strlen(str);
+printf("strlen =%d\n %d\n helloworld\n",b,a);
+
+    return 0;
+}
+
+
