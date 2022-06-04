67644f4808ddf1b13d5a76d57a37d2a969e5ffbf
amudalamadhusudhan <amadhusudhan2016@gmail.com>
2022-06-04 18:19:21 +0530

    pthread_attr_setdetachstate

--- thread/thread3.c
+++ thread/thread3.c
@@ -1,6 +1,7 @@
 #include <pthread.h>
 #include<stdio.h>
 pthread_t tid;
+pthread_attr_t attr;
 void *display(void *data)
 {
 
@@ -8,9 +9,13 @@ printf("this is a tread\n");
 }
 int main(int argv,char const argc[])
 {
+    pthread_attr_init(&attr);
+
+pthread_attr_setdetachstate(&attr,PTHREAD_CREATE_DETACHED);
+
 printf("before thread creation\n");
 pthread_create(&tid,NULL,display,NULL);
-//sleep(1);
+pthread_attr_destroy(&attr); //delocate resources 
 printf("after the thread creation\n");
 pthread_join(tid,NULL); //WAIT UNTILL display thread completed
 return 0;
