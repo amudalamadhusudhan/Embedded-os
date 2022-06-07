2144f49e48c8827f55cb113fcc0dc044a16ff80d
amudalamadhusudhan <amadhusudhan2016@gmail.com>
2022-06-07 10:45:46 +0530

    signaling semaphore

---
+++ signalingsem/signaling.c
@@ -0,0 +1,46 @@
+#include<stdio.h>
+#include<pthread.h>
+#include<semaphore.h>
+ 
+int a,b;
+sem_t sem_i2p,sem_p2i;
+void *inputThread(void *data)
+ {
+while(1)
+{
+    sem_wait(&sem_p2i);
+scanf("%d",&a);
+scanf("%d",&b);
+sem_post(&sem_i2p);
+}
+ }
+void *processingThread(void *data)
+{
+    int sum;
+while(1)
+{
+    sem_wait(&sem_i2p);
+sum=a+b;
+printf("sum :%d\n",sum);
+sem_post(&sem_p2i);
+}
+}
+
+
+
+int main(int argc ,char const *argv[])
+{
+
+pthread_t inputTid,procssingTid;
+sem_init(&sem_p2i,0,1);
+sem_init(&sem_i2p,0,0);
+pthread_create(&inputTid,NULL,inputThread,NULL);
+pthread_create(&procssingTid,NULL,processingThread,NULL);
+pthread_join(inputTid,NULL);
+pthread_join(procssingTid,NULL);
+
+
+return 0;
+
+
+}
