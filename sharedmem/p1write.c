340d6c54b269a328612dc536944ef01461d2b09f
amudalamadhusudhan <amadhusudhan2016@gmail.com>
2022-06-11 17:54:50 +0530

    shared memory

---
+++ sharedmem/p1write.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <sys/mman.h>
+#include <unistd.h>
+#include <sys/types.h>
+int *count = NULL;
+int main(int argc, char const *argv[])
+{
+    int shmfd;
+    shmfd = shm_open("/madhusudhanshm", O_CREAT | O_RDWR, S_IWGRP | S_IRUSR | S_IRGRP | S_IWUSR);
+    if (shmfd == -1)
+    {
+        perror("shm_open\n");
+        return EXIT_FAILURE;
+    }
+    ftruncate(shmfd, sizeof(int));
+    count = (int *)mmap(NULL, sizeof(int), PROT_READ | PROT_WRITE, MAP_SHARED, shmfd, 0); // shared memory mapped to our adrress space
+    while (1)
+    {
+        *count = *count + 1;
+        sleep(1);
+    }
+
+    return 0;
+}
